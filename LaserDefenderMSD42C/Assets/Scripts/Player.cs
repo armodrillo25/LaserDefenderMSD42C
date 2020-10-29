93f66d9c08219f132cddaa4db54198b02179d0d4
neilaquilina <6985772+neilaquilina@users.noreply.github.com>
2020-10-29 11:35:12 +0100

    DeltaTime, ViewPortToWorldPoint
    1. Made the game fram-rate independent
    2. Set up boundaries for the camera

--- LaserDefenderMSD42C/Assets/Scripts/Player.cs
+++ LaserDefenderMSD42C/Assets/Scripts/Player.cs
@@ -7,10 +7,15 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
+    //a variable that can be edited from Unity    
+    [SerializeField] float moveSpeed = 10f;
+
+    float xMin, xMax, yMin, yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
@@ -19,18 +24,38 @@ public class Player : MonoBehaviour
         Move();
     }
 
+    //setup the boundaries according to the camera
+    private void SetUpMoveBoundaries()
+    {
+        //get the camera from Unity
+        Camera gameCamera = Camera.main;
+
+        //xMin = 0  xMax = 1
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        //yMin = 0   yMax = 1
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+
+    }
+
     //moves the spaceship around
     private void Move()
     {
         //deltaX is updated with the movement in the x-axis (left and right)
-        var deltaX = Input.GetAxis("Horizontal");
-
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed ; 
+        
         //newXPos = current x-pos of Player
         //+ difference in X by keyboard Input
-        var newXPos = transform.position.x + deltaX;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        //the same as above but in the y-axis
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
 
         //update the position of the Player
-        this.transform.position = new Vector2(newXPos, transform.position.y);
+        this.transform.position = new Vector2(newXPos, newYPos);
             
     }
 
