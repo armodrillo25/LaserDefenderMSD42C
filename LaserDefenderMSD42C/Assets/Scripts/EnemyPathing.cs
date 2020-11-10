c37e13ee037305d0b296a758eecc0f8dd0dcab4e
neilaquilina <6985772+neilaquilina@users.noreply.github.com>
2020-11-10 15:44:52 +0100

    Enemy Movement on a Path
    1. waypoints and paths
    2. Lists and waypoints
    3. MoveTowards()

---
+++ LaserDefenderMSD42C/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    //saves the waypoint in which we want to go
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the start position of Enemy to the 1st waypoint position
+        transform.position = waypoints[waypointIndex].transform.position;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    private void EnemyMove()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            //set the targetPosition to the waypoint where we want to go
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            
+            //make sure that z axis = 0
+            targetPosition.z = 0f;
+
+            var enemyMovement = enemyMoveSpeed * Time.deltaTime;
+
+            //move Enemy from current position to targetPosition, at enemyMovement speed
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemyMovement);
+
+            //if enemy reaches the target position
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+
+        }
+
+        //if enemy reaches last waypoint
+        else
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+
+}
